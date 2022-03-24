3e7a7352aa4eb01fc6d386a20ebcc2cf8e701993
Fabio Duarte <mail.fabio.duarte@gmail.com>
2022-03-23 19:49:46 -0500

    feat: All unused directories from the boilerplate removed. Router configured to show 'HomeTwitterSamplePage.vue'

--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHashHistory } from "vue-router"
 
 const routes = [
-    {path: '/', component: () => import ('@/modules/demo/pages/Home.vue')},
-    {path: '/about', component: () => import ('@/modules/demo/pages/About.vue')},
-    {path: '/option', component: () => import ('@/modules/demo/pages/Option.vue')},
-    {path: '/:pathMatch(.*)*', component: () => import ('@/modules/shared/pages/NoFoundPage.vue')},
+    {path: '/', component: () => import ('@/modules/twitterSample/pages/HomeTwitterSamplePage.vue')},
+    // {path: '/about', component: () => import ('@/modules/demo/pages/About.vue')},
+    // {path: '/option', component: () => import ('@/modules/demo/pages/Option.vue')},
+    {path: '/:pathMatch(.*)*', component: () => import ('@/modules/twitterSample/pages/HomeTwitterSamplePage.vue')},
 ]
 
 const router = createRouter ({
